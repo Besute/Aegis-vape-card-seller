5f2261b4f048c6e331d01f2a7cfcc0ac6caddc8f
BORIS <boris200723@gmail.com>
2024-05-05 16:44:53 +0300

    Made OOP

--- src/js/script.js
+++ src/js/script.js
@@ -1,120 +1,42 @@
-const card = document.querySelector('.cardholder__card')
-const img = document.querySelector('.cardholder__img')
-const price = document.querySelector('.cardholder__price')
-const cost = document.querySelector('.cardholder__cost')
-const num = document.querySelector('.cardholder__number')
+class Hover {
+    constructor(target, num, img, price, cost, card) {
+        this.target = target
+        this.num = num
+        this.img = img
+        this.price = price
+        this.cost = cost
+        this.card = card
+    }
+    hover() {
+        this.target.addEventListener('mousemove', (e) => {
+            this.num.style.transition = this.card.style.transition = this.cost.style.transition = this.price.style.transition = this.img.style.transition = this.card.childNodes[1].style.transition = this.cost.style.transition = `all .1s`
+            this.num.style.opacity = this.price.style.opacity = '1'
+            this.card.childNodes[1].opacity = '0.4'
+            this.price.style.color = this.num.style.color = 'white'
+            this.cost.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.1) translate(-15px, -5px)`
+            this.img.style.filter = `drop-shadow(20px 15px 4px rgba(0, 0, 0, 0.25))`
+            this.cost.style.filter = this.num.style.filter =  `drop-shadow(5px 13px 4px rgba(0, 0, 0, 0.25))`
+            this.card.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg)`
+            this.img.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.15) translate(-30px, -20px)`
+            this.num.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.15)`
+        })
+    }
+    unhover() {
+        this.target.addEventListener('mouseout', () => {
+            this.price.style.transform = this.cost.style.transition = this.img.style.transition = this.card.style.transition = `all .23s`
+            this.card.style.transform = this.num.style.transform = this.img.style.transform = `rotateX(0) rotateY(0)`
+            this.card.childNodes[1].style.opacity = this.price.style.opacity = this.num.style.opacity = '0.4'
+            this.card.childNodes[1].style.color = 'white'
+            this.card.childNodes[1].style.transform = this.price.style.transform = this.cost.style.transform = this.num.style.opacity = 'scale(1)'
+            this.img.style.filter = `none`
+            this.cost.style.filter = this.num.style.filter = `none`
+        })
+    }
+}
 
-const color = `rgba(black, 0.25)`
+function creatNewObject(realCard) {
+    const card =  new Hover(realCard, realCard.childNodes[7], realCard.childNodes[1], realCard.childNodes[5].childNodes[1], realCard.childNodes[5], realCard.childNodes[3]) 
+    card.hover(), card.unhover()
+}
+creatNewObject(document.querySelector('.cardholder__realCard'))
 
-
-num.addEventListener('mousemove', (e) => {
-    num.style.transition = card.style.transition = cost.style.transition = price.style.transition = img.style.transition = card.childNodes[1].style.transition = cost.style.transition = `all .1s`
-    num.style.opacity = price.style.opacity = '1'
-    card.childNodes[1].opacity = '0.4'
-    price.style.color = num.style.color = 'white'
-    card.childNodes[1].style.transform = 'perspective(1000px)'
-    cost.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.1) translate(-15px, -5px)`
-    img.style.filter = `drop-shadow(20px 15px 4px rgba(0, 0, 0, 0.25))`
-    cost.style.filter = num.style.filter =  `drop-shadow(5px 13px 4px rgba(0, 0, 0, 0.25))`
-    card.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg)`
-    img.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.15) translate(-30px, -20px)`
-    num.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.15)`
-
-    card.childNodes.forEach(el => {
-        const a = el.className
-        if (a == undefined) return
-        let elem = document.querySelector(`.${el.className.split(' ').length > 1 ? el.className.split(' ')[1] : el.className.split(' ')[0]}`)
-        elem.classList.add('anim')
-    })
-})
-card.addEventListener('mousemove', (e) => {
-    num.style.transition = card.style.transition = cost.style.transition = price.style.transition = img.style.transition = card.childNodes[1].style.transition = cost.style.transition = `all .1s`
-    num.style.opacity = price.style.opacity = '1'
-    card.childNodes[1].opacity = '0.4'
-    price.style.color = num.style.color = 'white'
-    card.childNodes[1].style.transform = 'perspective(1000px)'
-    cost.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.1) translate(-15px, -5px)`
-    img.style.filter = `drop-shadow(20px 15px 4px rgba(0, 0, 0, 0.25))`
-    cost.style.filter = num.style.filter = `drop-shadow(5px 13px 4px rgba(0, 0, 0, 0.25))`
-    card.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg)`
-    img.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.15) translate(-30px, -20px)`
-    num.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.15)`
-
-    card.childNodes.forEach(el => {
-        const a = el.className
-        if (a == undefined) return
-        let elem = document.querySelector(`.${el.className.split(' ').length > 1 ? el.className.split(' ')[1] : el.className.split(' ')[0]}`)
-        elem.classList.add('anim')
-    })
-})
-img.addEventListener('mousemove', (e) => {
-    num.style.transition = card.style.transition = cost.style.transition = price.style.transition = img.style.transition = card.childNodes[1].style.transition = cost.style.transition = `all .1s`
-    num.style.opacity = price.style.opacity = '1'
-    card.childNodes[1].opacity = '0.4'
-    price.style.color = num.style.color = 'white'
-    card.childNodes[1].style.transform = 'perspective(1000px)'
-    cost.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.1) translate(-15px, -5px)`
-    img.style.filter = `drop-shadow(20px 15px 4px rgba(0, 0, 0, 0.25))`
-    cost.style.filter = num.style.filter = `drop-shadow(5px 13px 4px rgba(0, 0, 0, 0.25))`
-    card.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg)`
-    img.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.15) translate(-30px, -20px)`
-    num.style.transform = `perspective(1000px) rotateX(${(e.clientY-(window.innerHeight*0.5))/10}deg)rotateY(${(-e.clientX+(window.innerWidth*0.5))/11}deg) scale(1.15)`
-
-    card.childNodes.forEach(el => {
-        const a = el.className
-        if (a == undefined) return
-        let elem = document.querySelector(`.${el.className.split(' ').length > 1 ? el.className.split(' ')[1] : el.className.split(' ')[0]}`)
-        elem.classList.add('anim')
-    })
-})
-card.addEventListener('mouseout', (e) => {
-    card.style.transition = `all .23s`
-    img.style.transition = `all .23s`
-    price.style.transform = cost.style.transition = `all .23s`
-    card.style.transform = num.style.transform = img.style.transform = `rotateX(0) rotateY(0)`
-    card.childNodes[1].style.opacity = price.style.opacity = num.style.opacity = '0.4'
-    card.childNodes[1].style.color = 'white'
-    card.childNodes[1].style.transform = price.style.transform = cost.style.transform = num.style.opacity = 'scale(1)'
-    img.style.filter = `none`
-    cost.style.filter = num.style.filter = `none`
-
-    card.childNodes.forEach(el => {
-        const a = el.className
-        if (a == undefined || a == 'cardholder__text') return
-        let elem = document.querySelector(`.${el.className.split(' ').length > 1 ? el.className.split(' ')[1] : el.className.split(' ')[0]}`)
-        elem.classList.remove('anim')
-    })
-})
-img.addEventListener('mouseout', (e) => {
-    card.childNodes[1].style.transition = price.style.transform = cost.style.transition = card.style.transition = img.style.transition = `all .23s`
-    card.style.transform = num.style.transform = img.style.transform = `rotateX(0) rotateY(0)`
-    price.style.opacity = num.style.opacity = '0.4'
-    card.childNodes[1].style.color = 'white'
-    card.childNodes[1].style.transform = price.style.transform = cost.style.transform = num.style.opacity = 'scale(1)'
-    img.style.filter = `none`
-    cost.style.filter = num.style.filter = `none`
-
-    card.childNodes.forEach(el => {
-        const a = el.className
-        if (a == undefined || a == 'cardholder__text') return
-        let elem = document.querySelector(`.${el.className.split(' ').length > 1 ? el.className.split(' ')[1] : el.className.split(' ')[0]}`)
-        elem.classList.remove('anim')
-    })
-})
-
-num.addEventListener('mouseout', (e) => {
-    card.childNodes[1].style.transition = price.style.transform = cost.style.transition = card.style.transition = img.style.transition = `all .23s`
-    card.style.transform = num.style.transform = img.style.transform `rotateX(0) rotateY(0)`
-    price.style.opacity = num.style.opacity = '0.4'
-    card.childNodes[1].style.color = 'white'
-    card.childNodes[1].style.transform = price.style.transform = cost.style.transform = num.style.opacity = 'scale(1)'
-    img.style.filter = `none`
-    cost.style.filter = num.style.filter = `none`
-
-    card.childNodes.forEach(el => {
-        const a = el.className
-        if (a == undefined || a == 'cardholder__text') return
-        let elem = document.querySelector(`.${el.className.split(' ').length > 1 ? el.className.split(' ')[1] : el.className.split(' ')[0]}`)
-        elem.classList.remove('anim')
-    })
-})
